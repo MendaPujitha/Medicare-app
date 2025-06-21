e8e144cc5d22edb02a812c5a3928deff0ab32c4c
MendaPujitha <2004mendapujitha@gmail.com>
2025-06-21 19:42:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,25 @@
 // React frontend code placeholder
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [medications, setMedications] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/medications')
+      .then(res => res.json())
+      .then(data => setMedications(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Medication List</h1>
+      <ul>
+        {medications.map((med, index) => (
+          <li key={index}>{med.name} - {med.dosage}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
